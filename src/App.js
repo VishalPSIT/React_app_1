e44327029f18306884ee9938d9f83e7528e819a4
VishalPSIT <vishal.psit2213663@gmail.com>
2024-05-06 22:56:23 +0530

    day1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-
-
-
+import React, { useEffect, useState } from "react";
+import { apiUrl , filterData } from "./data";
+import Nav from "./components/nav";
+import Filter from "./components/filter";
+import Cards from "./components/cards";
+import { toast } from "react-toastify";
 function App() {
+  const [courses , setCourses] = useState(null);
+  useEffect(()=>{
+    const fetchData = async()=>{
+      try{
+        const res = await fetch(apiUrl);
+        const output = await res.json();
+        setCourses(output.data);
+      }
+      catch(error){
+        toast.error("something went wrong");
+      }
+    }
+  })
+
   return (
-    <div className="text-center underline ">
-      hello world
+    <div>
+      
+      <Nav />
+      <Filter filterData={filterData}/>
+      <Cards courses={courses}/>
+
     </div>
   );
 }
 
 export default App;
+//1.32.28
---
+++ src/components/nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div>
+            
+            <h1 className='text-center'>Top Courses</h1>
+            
+        </div>
+    );
+}
+
+export default Nav;
